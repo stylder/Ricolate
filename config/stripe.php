5546301faddee438fa830afb414ac9fc79fe6f4b
Stylder <stylder@gmail.com>
2017-06-05 22:06:25 -0500

    First commit

---
+++ config/stripe.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Stripe configuration
+ */
+
+return [
+    'secret_key' => env('SECRET_KEY', 'i2TvcuExkdSlXOqF3kdYZ9jbzd6cb1iK'),
+    'publishable_key' => env('PUBLISHABLE_KEY', 'i2TvcuExkdSlXOqF3kdYZ9jbzd6cb1iK'),
+];
